b123e11f722dc10d9c2c87e0e7b4a0449fb7cc99
sksalter503 <sksalter503@gmail.com>
2021-08-24 13:02:38 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Shane Salter
+ * Date: 2021/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+     printf("Hello World!\n");
+
+     return 0;
+ }
